cdafd5d5404d53dac7b4c387b50d272106353246
Quentin Boyer <quentin.boyer@atos.net>
2022-02-16 17:49:04 +0100

    more following ?

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     };
     neovim-overlay = {
       url = "github:nix-community/neovim-nightly-overlay";
-      #inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     # Inputs used by the home-manager module
