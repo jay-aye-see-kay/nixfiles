85e339ac835b9e1f4a9bf82640338780e55f3086
Jack Rose <user@jackrose.co.nz>
2022-06-15 19:26:18 +1000

    ditch xfce+i3, just use i3

--- features/i3-desktop.nix
+++ features/i3-desktop.nix
@@ -12,21 +12,23 @@
     backend = "glx";
   };
 
-  # basic xfce+i3 setup from the NixOS wiki
+  # basic i3 setup from the NixOS wiki
   services.xserver = {
     enable = true;
-    displayManager.defaultSession = "xfce";
-    desktopManager = {
-      xterm.enable = false;
-      xfce = {
-        enable = true;
-        noDesktop = true;
-        enableXfwm = false;
-      };
-    };
+    displayManager.defaultSession = "none+i3";
+    desktopManager.xterm.enable = false;
     windowManager.i3.enable = true;
   };
 
+  # If your settings aren't being saved for some applications (gtk3
+  # applications, firefox), like the size of file selection windows, or the
+  # size of the save dialog, you will need to enable dconf:
+  programs.dconf.enable = true;
+
   # Enable touchpad support (enabled default in most desktopManager).
   services.xserver.libinput.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    lxappearance
+  ];
 }
