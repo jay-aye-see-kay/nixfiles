204b463814b4cf993661adc74c208916c2e016c9
Jack Rose <user@jackrose.co.nz>
2022-07-01 11:44:31 +1000

    update emacs config

--- users/jack/emacs/default.nix
+++ users/jack/emacs/default.nix
@@ -2,6 +2,7 @@
 {
   programs.emacs = {
     enable = true;
+    package = pkgs.emacs28NativeComp;
     # extraConfig = lib.readFile ./emacs.el;
     extraPackages = epkgs: with epkgs; [
       all-the-icons
@@ -29,8 +30,9 @@
       nix-mode
       no-littering
       orderless
-      org-modern
+      org
       origami
+      plantuml-mode
       projectile
       restclient
       tree-sitter
@@ -41,7 +43,9 @@
       vertico
       visual-fill-column
       vterm
+      vundo
       which-key
+      yasnippet
     ];
   };
 
