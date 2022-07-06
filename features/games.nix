57cae2449962d1cda612c7f522f7779c7a1368cd
Jack Rose <user@jackrose.co.nz>
2022-07-06 22:35:44 +1000

    more steam

--- features/games.nix
+++ features/games.nix
@@ -1,4 +1,8 @@
 { config, pkgs, ... }:
 {
-  programs.steam.enable = true;
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
 }
