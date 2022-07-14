3a7874fe02801ba84377c3d28975434292d85416
Jack Rose <user@jackrose.co.nz>
2022-07-14 15:11:12 +1000

    arion, now with https

--- self-hosted-services/arion-compose.nix
+++ self-hosted-services/arion-compose.nix
@@ -1,39 +1,51 @@
-{ pkgs, ... }: {
-  config.project.name = "self hosted services";
+# Usage (it's mostly like docker-compose):
+# - all up: `arion up -d`
+# - show statuses: `arion ps`
+# - start/stop/down all: `arion <start/stop/down>`
+# - start single service: `arion start <service>`
+
+{ pkgs, ... }:
+let
+  projectName = "shs";
+  basePath = "/var/lib/shs/";
+  caddyfile = builtins.toFile "Caddyfile" ''
+    {
+      admin off
+      email "user+acme@jackrose.co.nz"
+    }
+
+    mealie.h.jackrose.co.nz {
+      reverse_proxy mealie:80
+    }
+  '';
+in
+{
+  config.project.name = projectName;
   config.services = {
-    # Thoughts:
-    # - this could easily be decomposed into multiple nix files (yay)
-    # - or it could be generated from a tangled org file (neat, but no linting/formatting)
-    # - it's completely indendent from the main config (yay!)
-
-    # Usage (it's mostly like docker-compose):
-    # - all up: `arion up -d`
-    # - show statuses: `arion ps`
-    # - start/stop/down all: `arion <start/stop/down>`
-    # - start single service: `arion start <service>`
-
-    # caddy setup
-    entry-point = {
-      service.useHostStore = true;
-      service.ports = [ "80:80" "2019:2019" ];
-      service.capabilities.SYS_ADMIN = true;
-
-      nixos.useSystemd = true;
-      nixos.configuration.boot.tmpOnTmpfs = true;
-
-      nixos.configuration.services.caddy = {
-        enable = true;
-        virtualHosts."http://test.h.jackrose.co.nz" = {
-          extraConfig = ''
-            reverse_proxy mealie:80
-          '';
-        };
+    # Reverse proxy
+    caddy = {
+      image = {
+        nixBuild = true;
+        enableRecommendedContents = true;
+        command = [ "${pkgs.caddy}/bin/caddy" "run" "--config=${caddyfile}" "--adapter=caddyfile" ];
+        contents = [ pkgs.cacert ];
+      };
+      service = {
+        container_name = "${projectName}-caddy";
+        useHostStore = true;
+        ports = [ "80:80" "443:443" "25565:25565" ];
+        volumes = [ "${basePath}/caddy-data:/.local/share/caddy" ];
       };
     };
 
-    # an example to test out hosting a docker container
-    mealie = {
-      service.image = "hkotel/mealie:v0.5.6";
+    mealie.service = {
+      container_name = "${projectName}-mealie";
+      image = "hkotel/mealie:v0.5.6";
+      volumes = [ "${basePath}/mealie-data:/app/data" ];
+      environment = {
+        TZ = "Australia/Melbourne";
+      };
     };
+
   };
 }
