2256e29aa3dae4c7c0fe83696c9713e20d51ba9b
Jack Rose <user@jackrose.co.nz>
2022-07-17 18:37:12 +1000

    add docker-compose to laptop

--- hosts/tui/default.nix
+++ hosts/tui/default.nix
@@ -69,6 +69,8 @@ in
     ];
   };
 
+  virtualisation.docker.enable = true;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
@@ -79,6 +81,7 @@ in
     btop
     chromium
     dbeaver
+    docker-compose
     fd
     firefox
     freecad
