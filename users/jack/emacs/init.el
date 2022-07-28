9ec6ac64d720989ff9e181a785e33f6ad6a6f3a1
Jack Rose <user@jackrose.co.nz>
2022-07-28 22:24:07 +1000

    add a typescript react mode (tree sitter not working, but eglot is happy)

--- users/jack/emacs/init.el
+++ users/jack/emacs/init.el
@@ -432,9 +432,21 @@
   (apheleia-global-mode +1))
 
 (use-package typescript-mode
+  :after tree-sitter
   :mode "\\.ts\\'\\|\\.tsx\\'"
   :hook (typescript-mode . eglot-ensure)
   :config
+  ;; we choose this instead of tsx-mode so that eglot can
+  ;; automatically figure out language for server see
+  ;; https://github.com/joaotavora/eglot/issues/624 and
+  ;; https://github.com/joaotavora/eglot#handling-quirky-servers
+  (define-derived-mode typescriptreact-mode typescript-mode
+    "TypeScript TSX")
+  ;; use our derived mode for tsx files
+  (add-to-list 'auto-mode-alist '("\\.tsx?\\'" . typescriptreact-mode))
+  ;; by default, typescript-mode is mapped to the treesitter typescript parser
+  ;; use our derived mode to map both .tsx AND .ts -> typescriptreact-mode -> treesitter tsx
+  (add-to-list 'tree-sitter-major-mode-language-alist '(typescriptreact-mode . tsx))
   (setq typescript-indent-level 2))
 
 (use-package nix-mode
