ba4ef2e03e9d8ae12bc7ec02c29be14f4bf7a6b1
Jack Rose <user@jackrose.co.nz>
2022-08-29 23:09:27 +1000

    break host-switch.sh into own script for granularity

---
+++ host-switch.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+if [ "$(uname)" = "Darwin" ]; then
+	echo "macos not supported, did you mean ./home-switch.sh instead?"
+	exit 1
+fi
+
+nixos-rebuild --use-remote-sudo switch --flake .#
