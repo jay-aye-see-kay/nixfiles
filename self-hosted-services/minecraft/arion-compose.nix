a9c7f67767cc1ca87d8f4fe5f7e827eff107f572
Jack Rose <user@jackrose.co.nz>
2022-09-08 10:18:07 +1000

    minecraft: remove pixelmon server and auto pause

--- self-hosted-services/minecraft/arion-compose.nix
+++ self-hosted-services/minecraft/arion-compose.nix
@@ -7,20 +7,6 @@ let
     MAX_MEMORY = "3G";
     TYPE = "PAPER";
     OVERRIDE_SERVER_PROPERTIES = "TRUE";
-
-    ENABLE_AUTOPAUSE = "TRUE";
-    # The maximum number of milliseconds a single tick may take before the server watchdog stops the
-    # server with the message, disabled because we're using autopause.
-    MAX_TICK_TIME = "-1";
-    # default 600 (seconds) describes the time between server start and the pausing of the process,
-    # when no client connects inbetween (read as timeout initialized)
-    AUTOPAUSE_TIMEOUT_INIT = "60";
-    # default 3600 (seconds) describes the time between the last client disconnect and the pausing
-    # of the process (read as timeout established)
-    AUTOPAUSE_TIMEOUT_EST = "60";
-    # default 120 (seconds) describes the time between knocking of the port (e.g. by the main menu
-    # ping) and the pausing of the process, when no client connects inbetween (read as timeout knocked)
-    AUTOPAUSE_TIMEOUT_KN = "30";
   };
 
   # for available versions see:
@@ -32,16 +18,6 @@ let
     {
       id = "rosalies-swamp";
       name = "Rosalie's Swamp";
-    }
-    {
-      id = "pixelmon";
-      name = "Pixelmon";
-      javaVersion = "java8";
-      envConfig = {
-        VERSION = "1.12.2";
-        TYPE = "CURSEFORGE";
-        CF_SERVER_MOD = "serverpack836.zip";
-      };
     }
   ];
 
@@ -58,7 +34,7 @@ let
       image = "itzg/minecraft-server:${javaVersion}";
       restart = "unless-stopped";
       environment = baseEnvConfig
-        // { MOTD = "${name} [refresh before joining]"; }
+        // { MOTD = "${name}"; }
         // envConfig;
       volumes = [
         "/data/minecraft/${id}:/data"
