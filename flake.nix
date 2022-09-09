9da7641d5b64d23d1714c5e29e550d81bf61b725
Jack Rose <user@jackrose.co.nz>
2022-09-09 21:58:02 +1000

    reduce some duplication in main flake

--- flake.nix
+++ flake.nix
@@ -26,67 +26,28 @@
     let
       username = "jack";
 
-      # x86 linux setup
-      system = "x86_64-linux";
-      overlayUnstable = final: prev: {
-        unstable = import nixpkgs-unstable {
+      mkPkgs = system:
+        let
+          overlayUnstable = final: prev: {
+            unstable = import nixpkgs-unstable {
+              inherit system;
+              config.allowUnfree = true;
+            };
+          };
+          nodePkgsOverlay = final: prev: {
+            customNodePackages = import ./node-packages/default.nix { pkgs = prev; };
+          };
+        in
+        import nixpkgs {
           inherit system;
-          config.allowUnfree = true;
+          config = { allowUnfree = true; };
+          overlays = [
+            overlayUnstable
+            emacs-overlay.overlay
+            nodePkgsOverlay
+            neovim-flake.overlays.${system}.default
+          ];
         };
-      };
-      nodePkgsOverlay = final: prev: {
-        customNodePackages =
-          import ./node-packages/default.nix { pkgs = prev; };
-      };
-
-      pkgs = import nixpkgs {
-        inherit system;
-        config = { allowUnfree = true; };
-        overlays = [
-          overlayUnstable
-          emacs-overlay.overlay
-          nodePkgsOverlay
-          neovim-flake.overlays.${system}.default
-        ];
-      };
-
-      # aarch64 setup (used by VM on m1)
-      systemAarch64 = "aarch64-linux";
-      overlayUnstableAarch64 = final: prev: {
-        unstable = import nixpkgs-unstable {
-          system = systemAarch64;
-          config.allowUnfree = true;
-        };
-      };
-      pkgsAarch64 = import nixpkgs {
-        system = systemAarch64;
-        config = { allowUnfree = true; };
-        overlays = [
-          overlayUnstableAarch64
-          emacs-overlay.overlay
-          nodePkgsOverlay
-          neovim-flake.overlays.${systemAarch64}.default
-        ];
-      };
-
-      # x86 Darwin setup (why does this seem to work for home manager on aarch64-darwin?)
-      systemDarwin = "x86_64-darwin";
-      overlayUnstableDarwin = final: prev: {
-        unstable = import nixpkgs-unstable {
-          system = systemDarwin;
-          config.allowUnfree = true;
-        };
-      };
-      pkgsDarwin = import nixpkgs {
-        system = systemDarwin;
-        config = { allowUnfree = true; };
-        overlays = [
-          overlayUnstableDarwin
-          emacs-overlay.overlay
-          nodePkgsOverlay
-          neovim-flake.overlays.${systemDarwin}.default
-        ];
-      };
 
       lib = nixpkgs.lib;
       commonHomeManagerImports = [
@@ -105,61 +66,69 @@
       formatter.aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.nixpkgs-fmt;
 
       homeManagerConfigurations.${username} =
+        let system = "x86_64-linux"; in
         home-manager.lib.homeManagerConfiguration {
-          inherit system pkgs username;
+          inherit system username;
+          pkgs = mkPkgs system;
           homeDirectory = "/home/${username}";
           configuration.imports = linuxHomeManagerImports;
           extraSpecialArgs = {
+            # WIP not using this, but it's cool I can pass it through
             jdr.neovim-packages = neovim-flake.extraPackages.${system};
           };
         };
 
       homeManagerConfigurations."${username}-aarch64" =
+        let system = "aarch64-linux"; in
         home-manager.lib.homeManagerConfiguration {
-          inherit username;
-          system = systemAarch64;
-          pkgs = pkgsAarch64;
+          inherit system username;
+          pkgs = mkPkgs system;
           homeDirectory = "/home/${username}";
           configuration.imports = linuxHomeManagerImports;
         };
 
       homeManagerConfigurations."${username}-mbp" =
+        let system = "aarch64-darwin"; in
         home-manager.lib.homeManagerConfiguration {
-          inherit username;
-          pkgs = pkgsDarwin;
-          system = systemDarwin;
+          inherit system username;
+          pkgs = mkPkgs system;
           homeDirectory = "/Users/${username}";
           configuration.imports = darwinHomeManagerImports;
         };
 
       nixosConfigurations = {
-        tui = lib.nixosSystem {
-          inherit system pkgs;
-          modules = [
-            nixos-hardware.nixosModules.lenovo-thinkpad-x1-6th-gen
-            ./hosts/tui
-            ./features/fonts.nix
-            ./features/games.nix
-            ./features/i3-desktop.nix
-            ./features/key-remapping.nix
-          ];
-        };
+        tui = let system = "x86_64-linux"; in
+          lib.nixosSystem {
+            inherit system;
+            pkgs = mkPkgs system;
+            modules = [
+              nixos-hardware.nixosModules.lenovo-thinkpad-x1-6th-gen
+              ./hosts/tui
+              ./features/fonts.nix
+              ./features/games.nix
+              ./features/i3-desktop.nix
+              ./features/key-remapping.nix
+            ];
+          };
 
-        kakapo = lib.nixosSystem {
-          inherit system pkgs;
-          modules = [
-            sops-nix.nixosModules.sops
-            ./secrets/sops.nix
-            ./hosts/kakapo
-            ./web-services
-          ];
-        };
+        kakapo = let system = "x86_64-linux"; in
+          lib.nixosSystem {
+            inherit system;
+            pkgs = mkPkgs system;
+            modules = [
+              sops-nix.nixosModules.sops
+              ./secrets/sops.nix
+              ./hosts/kakapo
+              ./web-services
+            ];
+          };
 
-        moa = lib.nixosSystem {
-          system = systemAarch64;
-          pkgs = pkgsAarch64;
-          modules = [ ./hosts/moa ./features/fonts.nix ];
-        };
+        moa = let system = "aarch64-linux"; in
+          lib.nixosSystem {
+            inherit system;
+            pkgs = mkPkgs system;
+            modules = [ ./hosts/moa ./features/fonts.nix ];
+          };
 
       };
     };
