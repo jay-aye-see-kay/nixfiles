5088d887d2afba55589608983652256d5fdcd8a2
Jack Rose <user@jackrose.co.nz>
2022-12-30 10:13:56 +1100

    change jump to fn to use tree-sitter

--- init.lua
+++ init.lua
@@ -768,6 +768,14 @@ require("nvim-treesitter.configs").setup({
 				["<leader>ph"] = "@parameter.inner",
 			},
 		},
+		move = {
+			enable = true,
+			set_jumps = true,
+			goto_next_start = { ["]m"] = "@function.outer" },
+			goto_next_end = { ["]M"] = "@function.outer" },
+			goto_previous_start = { ["[m"] = "@function.outer" },
+			goto_previous_end = { ["[M"] = "@function.outer" },
+		},
 	},
 })
 -- }}}
