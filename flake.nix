3b8cf9e2c5fa2174145bb80abe498e4b38edb8d6
Jack Rose <user@jackrose.co.nz>
2023-02-27 01:45:52 +1100

    add custom justfile grammar

--- flake.nix
+++ flake.nix
@@ -124,7 +124,18 @@
             vim-json
             jsonc-vim
 
-            (nvim-treesitter.withPlugins (_: nvim-treesitter.allGrammars))
+            (nvim-treesitter.withPlugins (_: nvim-treesitter.allGrammars ++ [
+              (pkgs.tree-sitter.buildGrammar {
+                language = "just";
+                version = "8af0aab";
+                src = pkgs.fetchFromGitHub {
+                  owner = "IndianBoy42";
+                  repo = "tree-sitter-just";
+                  rev = "8af0aab79854aaf25b620a52c39485849922f766";
+                  sha256 = "sha256-hYKFidN3LHJg2NLM1EiJFki+0nqi1URnoLLPknUbFJY=";
+                };
+              })
+            ]))
             nvim-treesitter-textobjects
             nvim-ts-autotag
             playground # tree-sitter playground
