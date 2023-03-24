6a87dc3a6d7112ef57877657724c1ffd1c8a0f26
Jack Rose <user@jackrose.co.nz>
2023-03-25 03:53:39 +1100

    add keymaps and file finding/creation so it's usable now

--- config/lua/_cfg/ai_buf_chat.lua
+++ config/lua/_cfg/ai_buf_chat.lua
@@ -1,11 +1,16 @@
---[[
--- okay so it works!
--- auto save when done
--- keybind to execute
--- shortcut to open last and new chat window
--- loading state
--- maybe a telescope shortcut to
---]]
+local initial_text = [[
+<!--​ 🔧 settings ​-->
+
+```json
+{ "model": "gpt-3.5-turbo" }
+```
+
+<!--​ 💻 system ​-->
+
+You're a programming assistant that responds with brief and accurate explanations, preferring code snippets where appropriate
+<!--​ 👤 user ​-->
+]]
+
 local M = {}
 
 M.markers = {
@@ -22,8 +27,12 @@ M.marker_lines = {
 	[M.markers.assistant] = "assistant",
 }
 
-M.default_settings = {
-	model = "gpt-3.5-turbo",
+M.config = {
+	chats_dir = "~/notes/ai-chats",
+	initial_text = initial_text,
+	default_settings = {
+		model = "gpt-3.5-turbo",
+	},
 }
 
 -- {{{ helper functions
@@ -37,6 +46,22 @@ end
 M.append_line_to_buffer = function(bufnr, line)
 	local line_count = vim.api.nvim_buf_line_count(bufnr)
 	vim.api.nvim_buf_set_lines(bufnr, line_count, line_count, false, { line })
+end
+M.delete_last_line_in_buffer = function(bufnr)
+	local last_line_num = vim.api.nvim_buf_line_count(bufnr)
+	vim.api.nvim_buf_set_lines(bufnr, last_line_num - 1, last_line_num, false, {})
+end
+M.read_files_in_dir = function(path)
+	if vim.fn.isdirectory(vim.fs.normalize(path)) == 0 then
+		return {}
+	end
+	local dir_contents = {}
+	for name, type in vim.fs.dir(path) do
+		if type == "file" then
+			table.insert(dir_contents, name)
+		end
+	end
+	return dir_contents
 end
 -- }}}
 
@@ -89,7 +114,7 @@ M.sections_to_api_format = function(sections)
 		settings_data = M.parse_settings_section(sections[1])
 	end
 
-	return vim.tbl_extend("force", M.default_settings, settings_data, { messages = messages })
+	return vim.tbl_extend("force", M.config.default_settings, settings_data, { messages = messages })
 end
 
 --- read an ai chat buffer into lines and convert it to a conversation we can send to the api
@@ -103,8 +128,11 @@ end
 -- TODO pass callback, non-blocking
 -- TODO set loading/readonly state
 M.send_api = function(bufnr)
+	bufnr = bufnr or vim.api.nvim_get_current_buf()
 	local url = "https://api.openai.com/v1/chat/completions"
 	local key = vim.env.OPENAI_API_KEY
+	M.append_line_to_buffer(bufnr, "")
+	M.append_line_to_buffer(bufnr, "Loading...")
 	require("plenary.job")
 		:new({
 			command = "curl",
@@ -130,7 +158,7 @@ M.send_api = function(bufnr)
 					and out.choices[1].message ~= nil
 					and out.choices[1].message.content ~= nil
 				then
-					M.append_line_to_buffer(bufnr, "")
+					M.delete_last_line_in_buffer(bufnr)
 					M.append_line_to_buffer(bufnr, M.markers.assistant)
 					M.append_line_to_buffer(bufnr, "")
 					local out_lines = M.split_into_lines(out.choices[1].message.content)
@@ -149,6 +177,68 @@ M.send_api = function(bufnr)
 		:start()
 end
 
--- M.send_api(129)
+M.get_last_chat_filename = function()
+	local chat_files = M.read_files_in_dir(M.config.chats_dir)
+	local valid_chat_files = {}
+	for _, chat_file in ipairs(chat_files) do
+		local date, num = string.match(chat_file, "(%d%d%d%d%-%d%d%-%d%d)_(%d+)%.ai%-chat.md")
+		if date ~= nil and num ~= nil then
+			table.insert(valid_chat_files, chat_file)
+		end
+	end
+	table.sort(valid_chat_files)
+	local last_chat_file = valid_chat_files[#valid_chat_files]
+	if last_chat_file == nil then
+		print("Not chats found")
+	end
+	return last_chat_file
+end
+
+M.get_new_chat_filename = function()
+	local todays_date = os.date("%Y-%m-%d", os.time())
+	local new_num = 1
+
+	local last_chat_file = M.get_last_chat_filename()
+	if last_chat_file then
+		local date, num = string.match(last_chat_file, "(%d%d%d%d%-%d%d%-%d%d)_(%d+)%.ai%-chat.md")
+		if date ~= nil and date == todays_date and num ~= nil then
+			new_num = tonumber(num) + 1
+		end
+	end
+
+	local new_num_str = string.format("%04d", new_num)
+	return todays_date .. "_" .. new_num_str .. ".ai-chat.md"
+end
+
+M.open_last_chat = function()
+	local filename = M.get_last_chat_filename()
+	if filename then
+		filename = vim.fs.normalize(filename)
+		vim.cmd("edit " .. filename)
+	end
+end
+
+M.open_new_chat = function()
+	local filename = M.get_new_chat_filename()
+	if filename then
+		filename = vim.fs.normalize(filename)
+		vim.cmd("edit " .. filename)
+		M.write_initial_text()
+	end
+end
+
+M.write_initial_text = function(bufnr)
+	bufnr = bufnr or vim.api.nvim_get_current_buf()
+	local initial_lines = M.split_into_lines(M.config.initial_text)
+	for _, line in ipairs(initial_lines) do
+		M.append_line_to_buffer(bufnr, line)
+	end
+end
+
+M.setup = function()
+	vim.keymap.set("n", "<leader>cc", M.send_api, { desc = "send buffer to openai" })
+	vim.keymap.set("n", "<leader>cn", M.open_new_chat, { desc = "open new ai-chat buffer" })
+	vim.keymap.set("n", "<leader>cl", M.open_last_chat, { desc = "open last ai-chat buffer" })
+end
 
 return M
