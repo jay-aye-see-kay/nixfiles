824f805ac21551414ceb8fa8b05b0d9cc363364d
Jack Rose <user@jackrose.co.nz>
2023-04-10 11:23:27 +1000

    tidy up main flake.nix

--- flake.nix
+++ flake.nix
@@ -3,105 +3,26 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-
     home-manager.url = "github:nix-community/home-manager/release-22.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
     sops-nix.url = "github:Mic92/sops-nix";
-
     neovim-flake.url = "github:jay-aye-see-kay/neovim-flake";
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , nixpkgs-unstable
-    , nixos-hardware
-    , home-manager
-    , sops-nix
-    , neovim-flake
-    }:
+  outputs = { self, nixpkgs, nixos-hardware, home-manager, sops-nix, ... }@inputs:
     let
       inherit (nixpkgs) lib;
+      inherit ((import ./utils.nix inputs)) mkPkgs eachMySystem mkHmConfigMod;
 
       username = "jack";
-      mySystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
 
-      # Build an attrset of config for each mySystem with `system` as the keys
-      # e.g.
-      #     eachMySystem (system: "__${system}")
-      # will return:
-      #     {
-      #       "x86_64-linux": "__x86_64-linux";
-      #       "x86_64-darwn": "__x86_64-darwn";
-      #       "aarch64-linux": "__aarch64-linux";
-      #       "aarch64-darwin": "__aarch64-darwin";
-      #     }
-      eachMySystem = mkConfig: (lib.lists.fold
-        (system: accum: accum // { ${system} = mkConfig system; })
-        { }
-        mySystems);
-
-      # Build pkgs with all my overlays for a given system
-      mkPkgs = system:
-        let
-          nixpkgs-unstable-overlay = final: prev: {
-            unstable = import nixpkgs-unstable {
-              inherit system;
-              config.allowUnfree = true;
-            };
-          };
-          node-pkgs-overlay = final: prev: {
-            customNodePackages = import ./node-packages/default.nix { pkgs = prev; };
-          };
-          cultureamp-overlay = final: prev: {
-            silk-cli =
-              let
-                version = "2.28.0";
-                rev = "6e2bbcb5e4852041ac85ebaf7f0504fd3a3ade71";
-              in
-              prev.buildGoModule rec {
-                inherit version;
-                pname = "silk";
-                src = builtins.fetchGit {
-                  inherit rev;
-                  url = "ssh://git@github.com/cultureamp/silk.git";
-                };
-                ldflags = [ "-s" "-w" "-X main.version=${version}" "-X main.commit=${rev}" "-X main.builtBy=nix" ];
-                # To get a new sha: `vendorSha256 = prev.lib.fakeSha256`
-                vendorSha256 = "sha256-/1u/CDc0GNPQqv6gYE9x0Y+ZvtURXgurJssUZiJUlo0=";
-              };
-          };
-          authelia-overlay = final: prev: {
-            authelia = import ./authelia prev;
-          };
-        in
-        import nixpkgs {
-          inherit system;
-          config = { allowUnfree = true; };
-          overlays = [
-            nixpkgs-unstable-overlay
-            node-pkgs-overlay
-            cultureamp-overlay
-            authelia-overlay
-            neovim-flake.overlays.${system}.default
-          ];
-        };
-
+      mkHmConfig = home-manager.lib.homeManagerConfiguration;
       commonHomeManagerImports = [
         ./users/jack/home.nix
         ./users/jack/fish.nix
       ];
       linuxHomeManagerImports = commonHomeManagerImports ++ [ ./users/jack/i3 ];
       darwinHomeManagerImports = commonHomeManagerImports;
-
-      mkHmConfig = home-manager.lib.homeManagerConfiguration;
-
-      mkHmConfigMod = { username, isDarwin ? false }: [{
-        home.username = username;
-        home.homeDirectory = "/${if isDarwin then "Users" else "home"}/${username}";
-        home.stateVersion = "22.05";
-      }];
     in
     {
       formatter = eachMySystem (system: nixpkgs.legacyPackages.${system}.nixpkgs-fmt);
--- utils.nix
+++ utils.nix
@@ -1,26 +1,88 @@
-{
-  mkTraefikRoute = name: localUrl: {
-    http.routers.${name} = {
-      rule = "Host(`${name}.h.jackrose.co.nz`)";
-      entrypoints = "websecure";
-      service = "${name}";
-      tls.certResolver = "default";
-    };
-    http.services.${name}.loadBalancer.servers = [{
-      url = "${localUrl}";
-    }];
+#
+# This file is only useful for the main flake.nix file, it expects the flake inputs
+# as it's parameter
+#
+{ nixpkgs, nixpkgs-unstable, neovim-flake, ... }: rec {
+  #
+  # list of systems this config can support
+  #
+  mySystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+  #
+  # Build an attrset of config for each mySystem with `system` as the keys
+  # e.g.
+  #     eachMySystem (system: "__${system}")
+  # will return:
+  #     {
+  #       "x86_64-linux": "__x86_64-linux";
+  #       "x86_64-darwn": "__x86_64-darwn";
+  #       "aarch64-linux": "__aarch64-linux";
+  #       "aarch64-darwin": "__aarch64-darwin";
+  #     }
+  #
+  eachMySystem = mkConfig: (nixpkgs.lib.lists.fold
+    (system: accum: accum // { ${system} = mkConfig system; })
+    { }
+    mySystems);
+
+  #
+  # boilerplate for home manager config
+  #
+  mkHmConfigMod = { username, isDarwin ? false, stateVersion ? "22.05" }: [{
+    home.username = username;
+    home.homeDirectory = "/${if isDarwin then "Users" else "home"}/${username}";
+    home.stateVersion = stateVersion;
+  }];
+
+  #
+  # adding stuff that's not in nixpkgs
+  #
+  myPkgsOverlay = final: prev: {
+    # stuff from npm
+    customNodePackages = import ./node-packages/default.nix { pkgs = prev; };
+
+    # authelia (only used on pukeko)
+    authelia = import ./authelia prev;
+
+    # silk-cli (only used on cultureamp laptop)
+    silk-cli =
+      let
+        version = "2.28.0";
+        rev = "6e2bbcb5e4852041ac85ebaf7f0504fd3a3ade71";
+      in
+      prev.buildGoModule rec {
+        inherit version;
+        pname = "silk";
+        src = builtins.fetchGit {
+          inherit rev;
+          url = "ssh://git@github.com/cultureamp/silk.git";
+        };
+        ldflags = [ "-s" "-w" "-X main.version=${version}" "-X main.commit=${rev}" "-X main.builtBy=nix" ];
+        # To get a new sha: `vendorSha256 = prev.lib.fakeSha256`
+        vendorSha256 = "sha256-/1u/CDc0GNPQqv6gYE9x0Y+ZvtURXgurJssUZiJUlo0=";
+      };
   };
 
-  mkProtectedTraefikRoute = name: localUrl: {
-    http.routers.${name} = {
-      rule = "Host(`${name}.h.jackrose.co.nz`)";
-      entrypoints = "websecure";
-      service = "${name}";
-      tls.certResolver = "default";
-      middlewares = "authelia@file";
+  #
+  # Build `pkgs` with my overlays for a given system
+  #
+  mkPkgs = system:
+    let
+      nixpkgs-unstable-overlay = final: prev: {
+        unstable = import nixpkgs-unstable {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      };
+    in
+    import nixpkgs {
+      inherit system;
+      config = { allowUnfree = true; };
+      overlays = [
+        nixpkgs-unstable-overlay
+        myPkgsOverlay
+        neovim-flake.overlays.${system}.default
+      ];
     };
-    http.services.${name}.loadBalancer.servers = [{
-      url = "${localUrl}";
-    }];
-  };
+
 }
