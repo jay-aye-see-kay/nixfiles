b76803e94fca348c54979bc3f8e3f73af9332fb5
Jack Rose <user@jackrose.co.nz>
2023-06-01 09:21:48 +1000

    update neotree icons

--- config/lua/_cfg/files-and-term.lua
+++ config/lua/_cfg/files-and-term.lua
@@ -38,6 +38,46 @@ require("neo-tree").setup({
 			},
 		},
 	},
+
+	-- the below config fixes icons for nerdfonts v3, hopefully it won't be necessary for long
+	default_component_configs = {
+		icon = {
+			folder_empty = "󰜌",
+			folder_empty_open = "󰜌",
+		},
+		git_status = {
+			symbols = {
+				renamed = "󰁕",
+				unstaged = "󰄱",
+			},
+		},
+	},
+	document_symbols = {
+		kinds = {
+			File = { icon = "󰈙", hl = "Tag" },
+			Namespace = { icon = "󰌗", hl = "Include" },
+			Package = { icon = "󰏖", hl = "Label" },
+			Class = { icon = "󰌗", hl = "Include" },
+			Property = { icon = "󰆧", hl = "@property" },
+			Enum = { icon = "󰒻", hl = "@number" },
+			Function = { icon = "󰊕", hl = "Function" },
+			String = { icon = "󰀬", hl = "String" },
+			Number = { icon = "󰎠", hl = "Number" },
+			Array = { icon = "󰅪", hl = "Type" },
+			Object = { icon = "󰅩", hl = "Type" },
+			Key = { icon = "󰌋", hl = "" },
+			Struct = { icon = "󰌗", hl = "Type" },
+			Operator = { icon = "󰆕", hl = "Operator" },
+			TypeParameter = { icon = "󰊄", hl = "Type" },
+			StaticMethod = { icon = "󰠄 ", hl = "Function" },
+		},
+	},
+	source_selector = {
+		sources = {
+			{ source = "filesystem", display_name = " 󰉓 Files " },
+			{ source = "git_status", display_name = " 󰊢 Git " },
+		},
+	},
 })
 -- }}}
 
