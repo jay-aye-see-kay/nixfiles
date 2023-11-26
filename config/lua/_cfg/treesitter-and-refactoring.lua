6cef1af313cfc64892d3e22ae12dc636605cd0d6
Jack Rose <user@jackrose.co.nz>
2023-11-27 10:03:02 +1100

    fix deprication warning in ts commentstring

--- config/lua/_cfg/treesitter-and-refactoring.lua
+++ config/lua/_cfg/treesitter-and-refactoring.lua
@@ -11,7 +11,6 @@ require("nvim-treesitter.configs").setup({
 		},
 	},
 	playground = { enable = true },
-	context_commentstring = { enable = true },
 	textobjects = {
 		select = {
 			enable = true,
--- flake.nix
+++ flake.nix
@@ -159,8 +159,8 @@
             playground # tree-sitter playground
 
             # comments
-            { plugin = comment-nvim; config = "lua require('Comment').setup({ pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook() })"; }
-            nvim-ts-context-commentstring
+            comment-nvim
+            { plugin = nvim-ts-context-commentstring; config = "lua vim.g.skip_ts_context_commentstring_module = true"; }
 
             # lsp stuff
             nvim-lspconfig
