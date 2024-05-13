f299fc0913ce3978bd7b86f2dc79bc3297e3dab6
Jack Rose <user@jackrose.co.nz>
2024-05-13 15:35:13 +1000

    just ts grammar is built in now

--- flake.nix
+++ flake.nix
@@ -135,18 +135,7 @@
             vim-just
             typescript-nvim
 
-            (nvim-treesitter.withPlugins (_: nvim-treesitter.allGrammars ++ [
-              (pkgs.tree-sitter.buildGrammar {
-                language = "just";
-                version = "8af0aab";
-                src = pkgs.fetchFromGitHub {
-                  owner = "IndianBoy42";
-                  repo = "tree-sitter-just";
-                  rev = "8af0aab79854aaf25b620a52c39485849922f766";
-                  sha256 = "sha256-hYKFidN3LHJg2NLM1EiJFki+0nqi1URnoLLPknUbFJY=";
-                };
-              })
-            ]))
+            (nvim-treesitter.withPlugins (_: nvim-treesitter.allGrammars))
             nvim-treesitter-textobjects
             nvim-ts-autotag
             playground # tree-sitter playground
