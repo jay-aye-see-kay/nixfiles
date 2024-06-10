4cab3e05b14c5f7a0c9f2e41c318d8356c9e53e3
Jack Rose <user@jackrose.co.nz>
2024-06-10 16:56:16 +1000

    remove old neotree config

--- neovim/lua/_cfg/files-and-term.lua
+++ neovim/lua/_cfg/files-and-term.lua
@@ -36,47 +36,7 @@ plugins["neo-tree.nvim"] = {
 					["a"] = { "add", config = { show_path = "relative" } },
 					["c"] = { "copy", config = { show_path = "relative" } },
 					["m"] = { "move", config = { show_path = "relative" } },
-					["X"] = {
-						function(state)
-							local node = state.tree:get_node()
-							h.toggle_executable_bit(node.path)
-						end,
-						desc = "toggle exec",
-					},
 				},
-			},
-		},
-		-- the below config fixes icons for nerdfonts v3, hopefully it won't be necessary for long
-		default_component_configs = {
-			icon = {
-				folder_empty = "󰜌",
-				folder_empty_open = "󰜌",
-			},
-			git_status = {
-				symbols = {
-					renamed = "󰁕",
-					unstaged = "󰄱",
-				},
-			},
-		},
-		document_symbols = {
-			kinds = {
-				File = { icon = "󰈙", hl = "Tag" },
-				Namespace = { icon = "󰌗", hl = "Include" },
-				Package = { icon = "󰏖", hl = "Label" },
-				Class = { icon = "󰌗", hl = "Include" },
-				Property = { icon = "󰆧", hl = "@property" },
-				Enum = { icon = "󰒻", hl = "@number" },
-				Function = { icon = "󰊕", hl = "Function" },
-				String = { icon = "󰀬", hl = "String" },
-				Number = { icon = "󰎠", hl = "Number" },
-				Array = { icon = "󰅪", hl = "Type" },
-				Object = { icon = "󰅩", hl = "Type" },
-				Key = { icon = "󰌋", hl = "" },
-				Struct = { icon = "󰌗", hl = "Type" },
-				Operator = { icon = "󰆕", hl = "Operator" },
-				TypeParameter = { icon = "󰊄", hl = "Type" },
-				StaticMethod = { icon = "󰠄 ", hl = "Function" },
 			},
 		},
 		source_selector = {
