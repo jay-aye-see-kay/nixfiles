2bbcd7ab38397ca3040b5e1527df075b714df137
Jack Rose <user@jackrose.co.nz>
2026-02-02 10:47:26 +1100

    switch to github regular contrast, it's clearer

--- dots/.config/nvim/lua/plugins/colorscheme.lua
+++ dots/.config/nvim/lua/plugins/colorscheme.lua
@@ -6,7 +6,7 @@ return {
 		priority = 1000,
 		config = function()
 			require("github-theme").setup({})
-			vim.cmd("colorscheme github_light_high_contrast")
+			vim.cmd("colorscheme github_light")
 		end,
 	},
 }
