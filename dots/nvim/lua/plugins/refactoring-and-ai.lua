36a6d075621b90d9d1760425337f0766f14665f5
Jack Rose <user@jackrose.co.nz>
2026-03-30 10:35:45 +1100

    add agentic.nvim (see if I like it)

--- dots/nvim/lua/plugins/refactoring-and-ai.lua
+++ dots/nvim/lua/plugins/refactoring-and-ai.lua
@@ -60,6 +60,37 @@ return {
 	},
 
 	{
+		"carlos-algms/agentic.nvim",
+		opts = { provider = "opencode-acp" },
+		keys = {
+			{
+				"<C-\\>",
+				function()
+					require("agentic").toggle()
+				end,
+				mode = { "n", "v", "i" },
+				desc = "Toggle Agentic Chat",
+			},
+			{
+				"<C-'>",
+				function()
+					require("agentic").add_selection_or_file_to_context()
+				end,
+				mode = { "n", "v" },
+				desc = "Add file or selection to Agentic to Context",
+			},
+			{
+				"<C-,>",
+				function()
+					require("agentic").new_session()
+				end,
+				mode = { "n", "v", "i" },
+				desc = "New Agentic Session",
+			},
+		},
+	},
+
+	{
 		"stevearc/quicker.nvim",
 		ft = "qf",
 		---@module "quicker"
